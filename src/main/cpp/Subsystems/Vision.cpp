53b68a2f716bf81920d061e2b5ccdc21d9ac278b
Jon <jon-bassi@users.noreply.github.com>
2024-02-08 18:06:57 -0800

    added wrist angle computation

--- src/main/cpp/Subsystems/Vision.cpp
+++ src/main/cpp/Subsystems/Vision.cpp
@@ -2,10 +2,28 @@
 
 
 
-double Vision::ComputeAngle(frc::Translation2d curPos, frc::Translation2d targetPos)
+double Vision::ComputeRobotRotation(frc::Pose2d curPos, frc::Translation2d targetPos)
 {
     double x = units::inch_t(curPos.X()).value() - units::inch_t(targetPos.X()).value();
     double y = units::inch_t(curPos.Y()).value() - units::inch_t(targetPos.Y()).value();
     
     return atan2(y,x);
+}
+
+double Vision::ComputeWristAngle(frc::Pose2d curPos, frc::Translation2d targetPos)
+{
+    double x = units::inch_t(curPos.X()).value() - units::inch_t(targetPos.X()).value();
+    double y = units::inch_t(curPos.Y()).value() - units::inch_t(targetPos.Y()).value();
+
+    x = pow(x,2);
+    y = pow(y,2);
+
+    double dist = sqrt(x+y);
+
+    // linear function to compute optimal wrist angle
+    // assuming 70 deg at 0 dist and 30 deg at 421 in (opposing alliance line)
+    // math would be 70 - 0.095d = angle
+    // compute wrist multiplier
+    double wristMultiplier = (CONSTANT("WRIST_AT_MAX") - CONSTANT("WRIST_AT_ZERO")) / CONSTANT("MAX_SHOOT_DIST");
+    return CONSTANT("WRIST_AT_ZERO") - wristMultiplier * dist;
 }
