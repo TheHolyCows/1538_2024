85777b9e08bc6bf0598a88955206a2a35bb5ba68
haberkiran <135155528+haberkiran@users.noreply.github.com>
2024-02-21 18:46:35 -0800

    Added Vision Logic
    Added logic to Vision.cpp and modified Vision.h

--- src/main/cpp/Vision.cpp
+++ src/main/cpp/Vision.cpp
@@ -5,13 +5,58 @@ Vision::Vision()
 
 }
 
-frc::Pose2d Vision::GetRobotPose()
+Vision::PoseWithLatency Vision::GetRobotPose()
 {
-    nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumberArray("<botpose_wpiblue>",std::vector<double>(6));
+    nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumberArray("<botpose_wpiblue>",std::vector<double>(7));
+    std::vector<double> limelightValues = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumberArray("<botpose_wpiblue>",std::vector<double>(6));
+
+    frc::Pose2d pose2d { units::meter_t {limelightValues[0]},
+                         units::meter_t {limelightValues[1]},
+                         units::degree_t {limelightValues[5]} }; 	
+
+    double totalLatency = limelightValues[6];
+
+    Vision::PoseWithLatency poseWithLatency;
+    poseWithLatency.pose2d = pose2d;
+    poseWithLatency.totalLatency = totalLatency;
+
+    return poseWithLatency;
 }
 
-void Vision::SetLEDState()
+void Vision::SetLEDState(Vision::LEDState ledState)
 {
-    
+    m_LEDState = ledState;
 }
 
+void Vision::Handle()
+{
+    nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("ledMode", 4);
+
+
+
+    if (m_LEDState == LEDState::OFF)
+    {
+        // Use documentation to turn Limelight off
+    }
+    else if (m_LEDState == LEDState::BLINK_SLOW || m_LEDState == LEDState::BLINK_FAST)
+    {
+        double m_StateChangeTime = frc::Timer::GetFPGATimestamp().value();
+        double timeElapsed = frc::Timer::GetFPGATimestamp().value() - m_StateChangeTime;
+        
+        if (m_LEDState == LEDState::BLINK_SLOW)
+        {
+            if (timeElapsed >= CONSTANT("BLINK_SLOW_TIME"))
+            {
+                // flip LED boolean
+            }
+            
+        }
+        else if (m_LEDState == LEDState::BLINK_FAST)
+        {
+            if (timeElapsed >= CONSTANT("BLINK_FAST_TIME"))
+            {
+                // flip LED boolean
+            }
+        }
+    }
+}
--- src/main/cpp/Vision.h
+++ src/main/cpp/Vision.h
@@ -1,18 +1,33 @@
 #pragma once
 
+#include "Cowconstants.h"
 #include <frc/geometry/Pose2d.h>
 #include <networktables/NetworkTableInstance.h>
+#include <frc/Timer.h>
 
 class Vision {
 public:
-    Vision();
-    frc::Pose2d GetRobotPose();
-    void SetLEDState();
     enum class LEDState
     {
         OFF,
         BLINK_SLOW,
         BLINK_FAST
     };
+    
+    struct PoseWithLatency 
+    { 
+        frc::Pose2d pose2d;
+        double totalLatency;
+    };
+    
+    Vision();
+    PoseWithLatency GetRobotPose();
+    void SetLEDState(LEDState ledState);
+    void Handle();
+
 private:
+    LEDState m_LEDState;
+    LEDState m_LEDBlinkInterval;
+    double m_StateChangeTime;
+    bool m_IsLEDOn;
 };
