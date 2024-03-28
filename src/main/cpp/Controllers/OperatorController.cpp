461edb21d9ad1952b656fecc11df94c3a863341f
Jon <jon-bassi@users.noreply.github.com>
2024-03-27 19:57:37 -0700

    temporarily comment cubic interp

--- src/main/cpp/Controllers/OperatorController.cpp
+++ src/main/cpp/Controllers/OperatorController.cpp
@@ -66,20 +66,20 @@ void OperatorController::Handle(CowRobot *bot)
         frc::Pose2d lookaheadPose = bot->GetDrivetrain()->Odometry()->Lookahead(CONSTANT("POSE_LOOKAHEAD_TIME"))
                                                                     .value_or(bot->GetDrivetrain()->GetPose());
         double dist = bot->m_Vision->GetTargetDist(bot->m_Alliance, lookaheadPose);
-        // double wristSetpoint = bot->m_PivotRangeMap[dist] + wristBias;
-        double wristSetpoint = (CONSTANT("WRIST_AUTO_RANGING_A") * std::pow(dist, 3)) +
-                               (CONSTANT("WRIST_AUTO_RANGING_B") * std::pow(dist, 2)) +
-                               (CONSTANT("WRIST_AUTO_RANGING_C") * std::pow(dist, 1)) +
-                               (CONSTANT("WRIST_AUTO_RANGING_D") * std::pow(dist, 0));
+        double wristSetpoint = bot->m_PivotRangeMap[dist] + wristBias;
+        // double wristSetpoint = (CONSTANT("WRIST_AUTO_RANGING_A") * std::pow(dist, 3)) +
+        //                        (CONSTANT("WRIST_AUTO_RANGING_B") * std::pow(dist, 2)) +
+        //                        (CONSTANT("WRIST_AUTO_RANGING_C") * std::pow(dist, 1)) +
+        //                        (CONSTANT("WRIST_AUTO_RANGING_D") * std::pow(dist, 0));
 
-        if (dist < CONSTANT("PIVOT_RANGE_DIST_1"))
-        {
-            wristSetpoint = CONSTANT("PIVOT_RANGE_VALUE_1");
-        }
-        else if (dist > CONSTANT("PIVOT_RANGE_DIST_5"))
-        {
-            wristSetpoint = CONSTANT("PIVOT_RANGE_VALUE_5");
-        }
+        // if (dist < CONSTANT("PIVOT_RANGE_DIST_1"))
+        // {
+        //     wristSetpoint = CONSTANT("PIVOT_RANGE_VALUE_1");
+        // }
+        // else if (dist > CONSTANT("PIVOT_RANGE_DIST_5"))
+        // {
+        //     wristSetpoint = CONSTANT("PIVOT_RANGE_VALUE_5");
+        // }
 
         wristSetpoint += wristBias;
 
@@ -92,7 +92,7 @@ void OperatorController::Handle(CowRobot *bot)
             bot->m_Shooter->PrimeShooter(bot->m_ShooterRangeMap[dist]);
         }
 
-        // printf("bias: %f, dist: %f\n", wristBias, dist);
+        printf("bias: %f, dist: %f\n", wristBias, dist);
 
         // LED
         if (dist < CONSTANT("SHOOTING_THRESHOLD_DISTANCE") &&
